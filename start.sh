70dfa5a4065146ffc8cecee8a71b87973f052378
hph <mrhph@qq.com>
2023-11-12 20:40:32 +0800

    add gunicorn

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export PYTHONPATH="`pwd`/src:`pwd`/lib"
+export VFX_LOG_DIR="`pwd`/logs"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd`/lib"
 poetry run uvicorn vfxs.asgi:app --host 0.0.0.0 --port 8888
